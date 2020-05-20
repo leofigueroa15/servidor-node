e1c19390b191f33522d348500d2d0699593e90e4
leofigueroa15 <leofigueroa15@gmail.com>
2020-05-20 15:57:00 -0400

    sas

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
 app.listen(port, '0.0.0.0',() => {
-    console.log(`El servidor esta corriendo en el puerto ${port}`);
+    console.log(`El servidor esta corriendo en el puerto ${port}`); 
 });
