f558e3548067be39d41a04f22360af90f01d4229
leofigueroa15 <leofigueroa15@gmail.com>
2020-05-20 15:25:43 -0400

    segunda subida

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const conectarDB = require('./config/db');
+
+const cors = require('cors');
+
+const app = express();
+
+// Conectar a la base de datos
+conectarDB();
+
+//Habilitar Cors
+
+app.use(cors());
+
+//Habilitar express .json
+app.use(express.json({extended: true}));
+
+const PORT = process.env.PORT || 4000;
+ 
+
+app.use('/api/usuarios', require('./routes/usuarios'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/proyectos', require('./routes/proyectos')); 
+app.use('/api/tareas', require('./routes/tareas'));
+
+app.listen(PORT, () => {
+    console.log(`El servidor esta corriendo en el puerto ${PORT}`);
+});
